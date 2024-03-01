c9d804442896ed3a03c35a394c50eb7e20efc5b6
Anissa Ras <anissa.rasoanirinahj@gmail.com>
2024-03-01 06:42:07 +0100

    first commit fornt neworigin

--- src/Components/rubriqueSTD/RubriquesListe.tsx
+++ src/Components/rubriqueSTD/RubriquesListe.tsx
@@ -145,7 +145,7 @@ const RubriqueListe = () => {
                             <path strokeLinecap="round" strokeLinejoin="round"
                                   d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
-                        <span onClick={handleOpenDialog}>Ajouter une rubrique</span>
+                        <span onClick={handleOpenDialog}>Ajouter</span>
                     </Button>
                 </div>
 
@@ -204,7 +204,7 @@ const RubriqueListe = () => {
                                         <th scope="col"
                                             className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                             <button className="flex items-center gap-x-2">
-                                                <span>Designation</span>
+                                                <span>Désignation des rubriques</span>
 
                                                 <svg className="h-3" viewBox="0 0 10 11" fill="none"
                                                      xmlns="http://www.w3.org/2000/svg" onClick={toggleSortOrder}>
@@ -228,16 +228,7 @@ const RubriqueListe = () => {
 
                                         <th scope="col"
                                             className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                            <button className="flex items-center gap-x-2">
-                                                <span>  </span>
-
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                     viewBox="0 0 24 24"
-                                                     stroke-width="2" stroke="currentColor" className="w-4 h-4">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"/>
-                                                </svg>
-                                            </button>
+                                            Ordre
                                         </th>
 
                                         <th scope="col"
